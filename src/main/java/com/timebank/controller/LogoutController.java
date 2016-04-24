7fd3e4039f34670d5751d1d4efa3e99f50fd0de5
Narcisa Brailoiu <narcisa.brailoiu@gmail.com>
2016-04-24 17:22:38 +0300

    Logout

---
+++ src/main/java/com/timebank/controller/LogoutController.java
@@ -0,0 +1,25 @@
+package com.timebank.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping ("/logout")
+public class LogoutController {
+
+	@Autowired 
+	HttpSession session;
+	
+	
+    @RequestMapping (method = RequestMethod.GET)
+     public String logout(ModelMap modelMap ) {
+        session.invalidate();
+        return "login";
+     }
+
+}
